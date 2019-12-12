6c3327d6c60a60f3e70bbb7bff5bd44cbb235ab8
AlexCaSsJS <idlcass181@gmail.com>
2019-12-12 07:02:22 +0200

    JS

---
+++ JavaScript/first/first.js
@@ -0,0 +1,31 @@
+var name = "Alex";
+var message = "Hello " + name + "!";
+var timeout = 3000;
+var isNight = false;
+var isMorning = false;
+var nothing = null;
+
+
+var date = new Date();
+var hour = date.getHours();
+
+
+if(hour >= 20 || hour <= 6) {
+    alert("Noapte Buna!"); 
+    alert("Somn usor!");
+}
+else if(hour <= 11) {
+    alert("Buna dimineata!");
+}
+else if(hour <= 17) {
+    alert("Buna ziua!");
+}
+else {
+    alert("Buna seara!");
+}
+
+setTimeout(sayHello, timeout);
+function sayHello() {
+    // alert(message);
+    console.log(message);
+}
